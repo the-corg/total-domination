2be728ae4cd4b7ecbfc5b9a1d6274fd214473706
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-04-24 15:14:19 +0200

    Add model and view model for a to do item

---
+++ TotalDomination/Model/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace TotalDomination.Model
+{
+    /// <summary>
+    /// Model class for a To do item
+    /// </summary>
+    public class Todo
+    {
+        public required string Title { get; set; }
+        public int Frequency { get; set; } = 1;
+        public DateOnly? LastDone { get; set; }
+
+    }
+}
---
+++ TotalDomination/ViewModel/TodoViewModel.cs
@@ -0,0 +1,77 @@
+﻿using TotalDomination.Model;
+
+namespace TotalDomination.ViewModel
+{
+    /// <summary>
+    /// View model for a single To do item
+    /// </summary>
+    public class TodoViewModel : BaseViewModel
+    {
+        private Todo _model;
+
+        public TodoViewModel(Todo model)
+        {
+            _model = model;
+        }
+
+        /// <summary>
+        /// Title of the To do item
+        /// </summary>
+        public string Title
+        {
+            get => _model.Title;
+            set
+            {
+                if (value == _model.Title)
+                    return;
+
+                _model.Title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Frequency of the To do item
+        /// </summary>
+        public int Frequency
+        {
+            get => _model.Frequency;
+            set
+            {
+                if (value == _model.Frequency)
+                    return;
+
+                _model.Frequency = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// The date when the To do item was last marked as done
+        /// </summary>
+        public DateOnly? LastDone
+        {
+            get => _model.LastDone;
+            set
+            {
+                if (value == _model.LastDone)
+                    return;
+
+                _model.LastDone = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Shows whether the To do item was done today
+        /// </summary>
+        public bool IsDone { get; set; }
+
+        /// <summary>
+        /// Shows whether the To do item is present in the current todo list
+        /// </summary>
+        public bool IsActive { get; set; }
+
+
+    }
+}
