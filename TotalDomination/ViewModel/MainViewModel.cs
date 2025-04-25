a9be408d644427a028ea8da6cebfd7c7a3a0bc88
Alexandra Sidorova <198761779+the-corg@users.noreply.github.com>
2025-04-25 02:11:21 +0200

    Implement basic ListBox with binding; improve styles and colors

--- TotalDomination/ViewModel/MainViewModel.cs
+++ TotalDomination/ViewModel/MainViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
 using TotalDomination.Data;
+using TotalDomination.Model;
 using TotalDomination.Properties;
 
 namespace TotalDomination.ViewModel
@@ -24,6 +27,21 @@ namespace TotalDomination.ViewModel
             _fileManager = fileManager;
 
             SelectFileCommand = new DelegateCommand(execute => SelectFile());
+
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 1" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 2" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 3 sad sd fasd fsd sad f" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 4" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 5" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 6" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 7" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 8" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 9" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 10" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 11" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 12" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 13" }));
+            Todos.Add(new TodoViewModel(new Todo() { Added = DateOnly.MinValue, FileName = _todoFilePath, Title = "Task 14" }));
         }
 
         /// <summary>
@@ -31,9 +49,22 @@ namespace TotalDomination.ViewModel
         /// </summary>
         public async Task InitializeAsync()
         {
+            // First load JSON (async)
+            // If there's no JSON, create an empty one
+
+            if (string.IsNullOrEmpty(_todoFilePath))
+                return;
+
             if (File.Exists(_todoFilePath))
             {
-
+                // Load file here (async)
+                // Add everything that's not in JSON to JSON
+            }
+            else
+            {
+                MessageBox.Show("Could not find file: " + _todoFilePath, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                _todoFilePath = "";
+                OnPropertyChanged(nameof(TodoFileName));
             }
         }
 
@@ -41,6 +72,8 @@ namespace TotalDomination.ViewModel
 
 
         #region Public properties 
+
+        public ObservableCollection<TodoViewModel> Todos { get; set; } = new();
 
         /// <summary>
         /// Name of the To-do list file
@@ -58,7 +91,7 @@ namespace TotalDomination.ViewModel
 
         private void SelectFile()
         {
-            _todoFilePath = _fileManager.SelectTodoListFile();
+            _todoFilePath = _fileManager.SelectTodoListFile() ?? "";
             Settings.Default.TodoListFile = _todoFilePath;
             Settings.Default.Save();
             OnPropertyChanged(nameof(TodoFileName));
